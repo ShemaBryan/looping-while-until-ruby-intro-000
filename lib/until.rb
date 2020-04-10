a9e3a73742ee6a5cf325d394d2843f7eccf6c8f9
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-10 14:53:55 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -2,7 +2,7 @@ def using_until
   levitation_force = 6
 4.times do
 until levitation_force == 10
-  levitatation_force += 1
+  levitation_force += 1
   puts "Wingardium Leviosa"
 break if levitation_force >= 10
   end
