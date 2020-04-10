1724c711c47fae16871c08b9aa738179dccc6326
Shema Bryan <shema.bryan@jjay.cuny.edu>
2020-04-10 14:12:18 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,9 +1,10 @@
 def using_while
-levitation_force = 0
+levitation_force = 6
 
-levitation_force += 1
+
 puts "Wingardium Leviosa #{levitation_force}"
 while levitation_force < 10
+levitation_force += 1
 break if levitation_force >= 10
    end
 end
